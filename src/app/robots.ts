0310eb67f7d0b6e104c4ab9113cb8fe68ed511aa
Hop-Syder <daoudaabassichristian@gmail.com>
2026-04-04 03:26:22 +0100

    modified:   src/app/robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,3 +1,13 @@
+/**
+ * @author @hopsyder
+ * @organization Nexus Partners
+ * @description Configuration robots.txt pour l'indexabilité totale
+ * @created 2026-04-04
+ * @updated 2026-04-04
+ * 🌐 ceo.nexuspartners.xyz
+ * 📧 daoudaabassichristian@gmail.com
+ */
+
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
@@ -5,7 +15,9 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: '*',
             allow: '/',
+            disallow: '',
         },
         sitemap: 'https://nexus-partners.xyz/sitemap.xml',
     }
 }
+
